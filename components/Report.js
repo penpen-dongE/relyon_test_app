a518ca0fa364ca6158b6a30499ebaf498edf1184
penpen-dongE <dongheepark.dev@gmail.com>
2021-02-03 00:27:13 +0900

    report image add

--- components/Report.js
+++ components/Report.js
@@ -10,55 +10,62 @@ import Footer from './reportComponents/Footer';
 
 export default function Report() {
 
-    const IMG = [
-        {
-            id:'0',
-            img: require('./../image/1.jpg')
-        },
-        {
-            id:'1',
-            img: require('./../image/2.jpg')
-        },
-        {
-            id:'2',
-            img: require('./../image/3.jpg')
-        },
-        {
-            id:'3',
-            img: require('./../image/4.jpg')
-        },
-        // {
-        //     id:'4',
-        //     img: require('./../image/4.jpg')
-        // },
-        // {
-        //     id:'5',
-        //     img: require('./../image/5.jpg')
-        // },
-        // {
-        //     id:'6',
-        //     img: require('./../image/6.jpg')
-        // },
-    ]
+    // const IMG = [
+    //     {
+    //         id:'0',
+    //         img: require('./../image/1.jpg')
+    //     },
+    //     {
+    //         id:'1',
+    //         img: require('./../image/2.jpg')
+    //     },
+    //     {
+    //         id:'2',
+    //         img: require('./../image/3.jpg')
+    //     },
+    //     {
+    //         id:'3',
+    //         img: require('./../image/4.jpg')
+    //     },
+    //     {
+    //         id:'4',
+    //         img: require('./../image/4.jpg')
+    //     },
+    //     {
+    //         id:'5',
+    //         img: require('./../image/5.jpg')
+    //     },
+    //     {
+    //         id:'6',
+    //         img: require('./../image/6.jpg')
+    //     },
+    // ]
 
-    const Item = ({ img }) => {
-        console.log(img);
-        return(
-            <SafeAreaView >
-                <Image style={{ width: '100%', resizeMode:'contain' }}
-                        source = {img} />
-            </SafeAreaView>
-        )
-    }
+    // const Item = ({ img }) => {
+    //     console.log(img);
+    //     return(
+    //         <SafeAreaView >
+    //             <Image style={{ width: '100%', resizeMode:'contain' }}
+    //                     source = {img} />
+    //         </SafeAreaView>
+    //     )
+    // }
 
-    const renderItem = ({item}) => (
-        <Item img={item.img} />
-    )
+    // const renderItem = ({item}) => (
+    //     <Item img={item.img} />
+    // )
+
+    const dimensions = Dimensions.get('window');
+    const imageHeight = dimensions.width * 2.1;
+    const imageHeight1 = dimensions.width * 1.6;
+    const imageHeight2 = dimensions.width * 1.4;    
+    const imageHeight3 = dimensions.width * 2.6; 
+
     return (
         <View style={styles.container}>
             <Header />
             <Title />
-            <View style={styles.flatContainer}>
+            {/* <View style={styles.flatContainer}>
                 <FlatList
                     style = {styles.flat}
                     renderItem={renderItem}
@@ -69,53 +76,58 @@ export default function Report() {
                     //     <View style={{marginTop:-50}}/>
                     // )}
                 />
-            </View>
-            
-            {/* <ScrollView style={styles.scroll}> */}
-                {/* <View style={styles.content}> */}
-                    {/* <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[0].img} />
+            </View> */}
+            <SafeAreaView style={styles.scrollContainer}>
+                <ScrollView style={styles.scroll}>
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[1].img} />
+                    style={{ height: imageHeight, width: '100%', resizeMode: 'stretch'}}
+                    source = {require('./../image/0.jpg')} />
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[2].img} />
+                    style={{ height: imageHeight1, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/1.jpg')} />  
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[3].img} />
+                    style={{ height: imageHeight2, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/2.jpg')} />  
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[4].img} />
+                    style={{ height: imageHeight, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/3.jpg')} /> 
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[5].img} />
+                    style={{ height: imageHeight3, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/4.jpg')} />   
                     <Image 
-                        style={{ width: '100%', resizeMode:'contain'}}
-                        source = {IMG[6].img} /> */}
-                {/* </View> */}
-                
-            {/* </ScrollView>     */}
+                    style={{ height: imageHeight3, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/5.jpg')} /> 
+                    <Image 
+                    style={{ height: imageHeight, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/6.jpg')} />       
+                </ScrollView> 
+            </SafeAreaView> 
             <Footer />
         </View>     
     );
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 15,
         // width: '100%',
         backgroundColor: 'yellow',
         // justifyContent: 'center',
         // alignItems: 'center',
         // marginTop: Constants.statusBarHeight,
     },
+    scrollContainer:{
+        flex:1,
+        width: '100%',
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     flatContainer:{
         flex:1,
         justifyContent: "center",
     },
     scroll: {
-        // width: '90%',
+        width: '90%',
         backgroundColor: 'yellow',
     },
     content: {
