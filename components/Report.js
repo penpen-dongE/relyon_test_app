9607da62e5f9bbb6b37df78e8977ae0d7562f6cc
JiHye Lee <jihye0320@gmail.com>
2021-02-03 00:44:28 +0900

    2nd commit

--- components/Report.js
+++ components/Report.js
@@ -2,15 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, SafeAreaView} from 'react-native';
 import { Image } from 'react-native' ; 
-
+import { Dimensions } from 'react-native';
 
 export default function Report() {
+
+    const dimensions = Dimensions.get('window');
+    const imageHeight = dimensions.width * 2.1;
+    const imageHeight1 = dimensions.width * 1.6;
+    const imageHeight2 = dimensions.width * 1.4;    
+    const imageHeight3 = dimensions.width * 2.6; 
+
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.scroll}>
                 <Image 
-                    style={{ width: '100%', resizeMode:'stretch'}}
-                    source = {require('./../image/report.png')} />
+                    style={{ height: imageHeight, width: '100%', resizeMode: 'stretch'}}
+                    source = {require('./../image/0.jpg')} />
+                <Image 
+                    style={{ height: imageHeight1, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/1.jpg')} />  
+                <Image 
+                    style={{ height: imageHeight2, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/2.jpg')} />  
+                <Image 
+                    style={{ height: imageHeight, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/3.jpg')} /> 
+                <Image 
+                    style={{ height: imageHeight3, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/4.jpg')} />   
+                <Image 
+                    style={{ height: imageHeight3, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/5.jpg')} /> 
+                <Image 
+                    style={{ height: imageHeight, width: '100%', resizeMode:'stretch'}}
+                    source = {require('./../image/6.jpg')} />       
             </ScrollView>    
         </SafeAreaView>     
     );
@@ -26,7 +51,7 @@ const styles = StyleSheet.create({
     },
     scroll: {
         width: '90%',
-        backgroundColor: 'green',
+        backgroundColor: 'yellow',
     },
 
 });
